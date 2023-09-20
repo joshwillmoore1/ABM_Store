e04a15f490fec3a0b477854073dcd325757a93b4
Joshua Moore <45179849+joshwillmoore1@users.noreply.github.com>
2023-09-20 19:40:06 +0100

    Add files via upload

---
+++ CMakeFiles/chaste_project_Mammary.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chaste_project_Mammary.dir/src/Hello.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidCellCycleModels/OrganoidG1FixedCellCycleModel.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidForces/BiBuckleRandomMotionForce.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidForces/OrganoidSpringForce.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidForces/RandomMotionForce.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidKillers/LumenKiller.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidProliferativeTypes/BasalStemCellProliferativeType.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidProliferativeTypes/LumenERNegativeCellProliferativeType.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidProliferativeTypes/LumenERPositiveCellProliferativeType.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidProliferativeTypes/MyoEpiCellProliferativeType.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/BscOdeSystem.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/BscSrnModel.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/BscTrackingModifier.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/HexBscTrackingModifier.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/MamDeltaNotchOdeSystem.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/MamDeltaNotchSrnModel.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/MamDeltaNotchTrackingModifier.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/MamNDMAdaptiveTrackingModifier.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/MooreGridBscTrackingModifier.cpp.o"
+  "CMakeFiles/chaste_project_Mammary.dir/src/OrganoidSRN/NeuGridBscTrackingModifier.cpp.o"
+  "libchaste_project_Mammary.pdb"
+  "libchaste_project_Mammary.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chaste_project_Mammary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chaste/src/projects/Mammary
+# Build directory: /home/chaste/projects/Mammary
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
+subdirs("apps")
